d16957a5c6cd5ba8e7eadc1c454700b4e643c8b1
Setoju <setoju48@gmail.com>
2025-03-04 16:17:12 +0200

    fix edit post function

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,6 @@ class ProfilesController < ApplicationController
 
     def index
       @blog_posts = BlogPost.where(user_id: Current.user.id).order(created_at: :desc)
+      @editing_post = BlogPost.find_by(id: params[:editing_post_id]) if params[:editing_post_id].present?
     end
 end
